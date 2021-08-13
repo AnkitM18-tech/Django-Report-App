303a6a590b7c9c112467c4d6e286d4c608d66f3f
Ankit Kumar Mohanty <67118175+AnkitM18-tech@users.noreply.github.com>
2021-08-13 19:54:02 +0530

    Update settings.py

--- src/reports_proj/settings.py
+++ src/reports_proj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t!zj%-*ubd11)6ir)y#q707qg!31khsk!rzf+a%s34#!maoea('
+SECRET_KEY = 'THERE WILL BE A SECRET KEY WHEN YOU CREATE THE DJANGO APP'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
